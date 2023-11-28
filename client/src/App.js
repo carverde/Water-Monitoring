3e01ba7e0c2785d12803d85cfe39bd67628369d4
carverde <90798442+carverde@users.noreply.github.com>
2023-11-28 16:53:07 -0500

    settings page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,49 @@
 import React, { useState } from 'react';
 import SettingsPage from './components/SettingsPage';
 import DataView from './components/Connector';
+import firebase from './components/config';
 
 function App() {
   const [thresholds, setThresholds] = useState({
-    pH: 50,
-    turbidity: 30,
-    conductivity: 70,
-    temperature: 80,
+    pH: 3,
+    turbidity: 0,
+    conductivity: 0,
+    temperature: 25,
   });
 
   const [currentPage, setCurrentPage] = useState('home');
 
   const handleThresholdChange = (sensor, value) => {
-    setThresholds((prevThresholds) => ({ ...prevThresholds, [sensor]: value }));
+    // Convert the value to a float using parseFloat
+    const floatValue = parseFloat(value);
+  
+    // Check if the parsed value is a valid number and not NaN
+    if (!isNaN(floatValue) && typeof floatValue === 'number') {
+      setThresholds((prevThresholds) => ({ ...prevThresholds, [sensor]: floatValue }));
+  
+      // Update Firebase with the new threshold value based on the sensor type
+      switch (sensor) {
+        case 'pH':
+          firebase.database().ref('/Test/pH').set(floatValue);
+          break;
+        case 'conductivity':
+          firebase.database().ref('/Test/Cond').set(floatValue);
+          break;
+        case 'temperature':
+          firebase.database().ref('/Test/Temp').set(floatValue);
+          break;
+        case 'turbidity':
+          firebase.database().ref('/Test/Tur').set(floatValue);
+          break;
+        default:
+          break;
+      }
+    } else {
+      // Handle invalid input (non-numeric or NaN)
+      console.error(`Invalid input for ${sensor}: ${value}`);
+    }
   };
-
+  
   return (
 
     <div>
@@ -24,10 +52,10 @@ function App() {
         <button onClick={() => setCurrentPage('home')}>Data Center</button>
         <button onClick={() => setCurrentPage('settings')}>Threshold Control</button>
       </nav>
+      {currentPage === 'home' && (<DataView />)}
       {currentPage === 'settings' && (
         <SettingsPage thresholds={thresholds} onThresholdChange={handleThresholdChange} />
       )}
-        <DataView />
     </div>
   );
 };
--- client/src/components/SettingsPage.js
+++ client/src/components/SettingsPage.js
@@ -1,16 +1,20 @@
 // SettingsPage.js
 import React from 'react';
-import Slider from './Slider';
 
 const SettingsPage = ({ thresholds, onThresholdChange }) => {
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>Threshold</h1>
-      <div className="slider-container">
+      <div className="input-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {Object.entries(thresholds).map(([key, value]) => (
-          <div key={key}>
-            <h2>{key}</h2>
-            <Slider value={value} onChange={(e) => onThresholdChange(key, e.target.value)} />
+          <div key={key} className="input-box" style={{ margin: '10px' }}>
+            <label htmlFor={key} style={{ marginBottom: '5px' }}>{key}</label>
+            <input
+              type="number"
+              id={key}
+              value={value}
+              onChange={(e) => onThresholdChange(key, e.target.value)}
+            />
           </div>
         ))}
       </div>
