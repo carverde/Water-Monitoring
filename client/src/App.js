aa83910d9ce7d75d2fc01ff9863b0305ee3cb952
Luis Guerrero <guerrerol@ufl.edu>
2023-11-28 16:20:59 -0500

    Added Sign-In and Sign-Out Buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import SettingsPage from './components/SettingsPage';
 import DataView from './components/Connector';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
   const [thresholds, setThresholds] = useState({
@@ -16,6 +20,8 @@ function App() {
     setThresholds((prevThresholds) => ({ ...prevThresholds, [sensor]: value }));
   };
 
+  const { isLoading, error } = useAuth0();
+
   return (
 
     <div>
@@ -23,6 +29,14 @@ function App() {
         <h2>Fullstack IoT Cutie Pies</h2>
         <button onClick={() => setCurrentPage('home')}>Data Center</button>
         <button onClick={() => setCurrentPage('settings')}>Threshold Control</button>
+        {error && <p>Authentication Error</p>}
+        {!error && isLoading && <p>Loading...</p>}
+        {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+        </>
+      )}
       </nav>
       {currentPage === 'settings' && (
         <SettingsPage thresholds={thresholds} onThresholdChange={handleThresholdChange} />
